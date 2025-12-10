28df3f3e2bc49ffad0e805de7b8f384d50db0e2f
jacksonemmerich@gmail.com <jackson.emmerich@portovelho.ro.gov.br>
2025-12-10 14:25:05 -0400

    first commit

---
+++ vendas-app/src/components/layout/index.tsx
@@ -0,0 +1,30 @@
+
+const Layout: React.FC = () => {
+  return (
+    <div className="app">
+      <section className="main-content columns is-fullheight">
+        {
+          /* menu principal */
+        }
+        <div className="container column is-10">
+          <div className="section">
+            <div className="card">
+              <div className="card-header">
+                <p className="card-header-title">
+                  Cadastro
+                </p>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  Conteúdo
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Layout;  
---
+++ vendas-app/src/pages/meu-componente.tsx
@@ -0,0 +1,22 @@
+
+interface MensagemProps {
+  mensagem: number
+}
+
+const Mensagem: React.FC<MensagemProps> = (props) => {
+  return (
+    <div>
+      {props.mensagem}
+    </div>
+  ) 
+}
+
+const MeuComponente = () => {
+  return (
+    <div>
+      <Mensagem mensagem={10} />
+    </div>
+  )
+} 
+
+export default MeuComponente;
